f13b202eee9fc309905d95da05b9b9aaaeeffc6d
ryosukek <allrounder1983@gmail.com>
2015-08-19 09:40:50 +0000

    add posting microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ActiveRecord::Base
                       format: { with: VARID_EMAIL_REGEX },
                       uniqueness: { case_sensitive: false }
     has_secure_password
+    has_many :microposts
 end
